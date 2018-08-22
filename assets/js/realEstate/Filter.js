681894cef41886f58aa8643bfbbd360d5b543f75
amenraprod <amenraprod@gmail.com>
2018-08-22 11:12:12 -0400

    added two more selectors

--- assets/js/realEstate/Filter.js
+++ assets/js/realEstate/Filter.js
@@ -12,7 +12,13 @@ export default class Filter extends Component {
     return (<section className="Filter">
     <h4>Filter</h4>
     <select name="neighborhood" class="neighborhood">
-    <option>Teohotiuacan </option>
+    <option>Los Angeles </option>
+    </select>
+    <select name="housetypes" class="housetypes">
+    <option>Townhomes </option>
+    </select>
+    <select name="bedrooms" class="bedrooms">
+    <option>2 bedrooms </option>
     </select>
     </section>)
   }

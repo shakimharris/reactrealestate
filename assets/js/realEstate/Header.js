6bcb5c95d2dd680cddf68093956aabd409c647d6
amenraprod <amenraprod@gmail.com>
2018-08-22 03:27:11 -0400

    exported Header component

--- assets/js/realEstate/Header.js
+++ assets/js/realEstate/Header.js
@@ -1,7 +1,7 @@
 import React, { Component} from 'react'
 import ReactDOM from 'react-dom'
 
-class Header extends Component {
+export default class Header extends Component {
   constructor () {
     super()
     this.state = {

66e7ac7358eec3fea523291573e762cf61082951
amenraprod <amenraprod@gmail.com>
2018-08-22 09:21:05 -0400

    removed react-dom

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -1,6 +1,4 @@
 import React, { Component} from 'react'
-import ReactDOM from 'react-dom'
-
 export default class Header extends Component {
   constructor () {
     super()
@@ -8,23 +6,7 @@ export default class Header extends Component {
       name: 'Joe'
     }
   }
-  clickedBtn = () => {
-    console.log('swag')
-  }
-  render () {
-    return (<header>
-      <div className="Logo">Logo</div>
-      <nav>
-      <a href="#">create ads</a>
-      //<a href="#">log in</a>
-      <a href="#">about us</a>
-      <a href="#" className="register-btn">Register</a>
-       </nav>
 
-
-      </header>)
-  }
-}
 
 const app = document.getElementById('app')
 

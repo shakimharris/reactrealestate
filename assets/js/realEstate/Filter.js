c5a72c551d5caf6d58ac04f3292a7306ecde3156
amenraprod <amenraprod@gmail.com>
2018-08-23 01:01:35 -0400

    added filter to section classes

--- assets/js/realEstate/Filter.js
+++ assets/js/realEstate/Filter.js
@@ -11,21 +11,21 @@ export default class Filter extends Component {
   render () {
     return (<section className="Filter">
     <h4>Filter</h4>
-    <select name="neighborhood" class="neighborhood">
+    <select name="neighborhood" class="filters neighborhood">
       <option>Los Angeles </option>
     </select>
-    <select name="housetypes" class="housetypes">
+    <select name="housetypes" class="filters housetypes">
       <option>Townhomes </option>
     </select>
-    <select name="bedrooms" class="bedrooms">
+    <select name="bedrooms" class="filters bedrooms">
       <option>2 bedrooms </option>
     </select>
-    <div class="filters-prices">
+    <div class="filters prices">
       <span className="title"> Price </span>
       <input type="text" name="min-price" className="min-price"/>
       <input type="text" name="max-price" className="max-price"/>
    </div>
-    <div class="filters-floorspace">
+    <div class="filters floorspace">
         <span className="title"> Price </span>
         <input type="text" name="min-floorspace" className="min-floorspace"/>
         <input type="text" name="max-floorspace" className="max-floorspace"/>
